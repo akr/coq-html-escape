a3db69a402d1c18272afd3609a51f83098dc6320
Tanaka Akira <akr@fsij.org>
2019-06-10 19:44:12 +0900

    Update to Coq 8.9 with the latest codegen and monadification plugin.

--- coq/theories/gen.v
+++ coq/theories/gen.v
@@ -10,26 +10,26 @@ Require Import htmlescape.imp.
 
 Require Import codegen.codegen.
 
-Terminate Monomorphization html_escape_byte_table.
-Terminate Monomorphization cmpestri_ubyte_eqany_ppol_lsig.
-Terminate Monomorphization chars_to_escape.
-Terminate Monomorphization m128_of_bptr.
-Terminate Monomorphization bptradd.
-Terminate Monomorphization bptrget.
-Terminate Monomorphization bufaddmem.
-Terminate Monomorphization addn.
-Terminate Monomorphization subn.
-Terminate Monomorphization leq.
+CodeGen Terminate Monomorphization html_escape_byte_table.
+CodeGen Terminate Monomorphization cmpestri_ubyte_eqany_ppol_lsig.
+CodeGen Terminate Monomorphization chars_to_escape.
+CodeGen Terminate Monomorphization m128_of_bptr.
+CodeGen Terminate Monomorphization bptradd.
+CodeGen Terminate Monomorphization bptrget.
+CodeGen Terminate Monomorphization bufaddmem.
+CodeGen Terminate Monomorphization addn.
+CodeGen Terminate Monomorphization subn.
+CodeGen Terminate Monomorphization leq.
 
-Terminate Monomorphization cmpestrc_ubyte_eqany_ppol_lsig_bitmask.
-Terminate Monomorphization cmpestrm_ubyte_eqany_ppol_lsig_bitmask.
-Terminate Monomorphization lo64_of_m128.
+CodeGen Terminate Monomorphization cmpestrc_ubyte_eqany_ppol_lsig_bitmask.
+CodeGen Terminate Monomorphization cmpestrm_ubyte_eqany_ppol_lsig_bitmask.
+CodeGen Terminate Monomorphization lo64_of_m128.
 
-Monomorphization trec_html_escape.
-Monomorphization sse_html_escape.
-Monomorphization sse_html_escape2.
+CodeGen Monomorphization trec_html_escape.
+CodeGen Monomorphization sse_html_escape.
+CodeGen Monomorphization sse_html_escape2.
 
-GenCFile "gen/esc.c"
+CodeGen GenCFile "gen/esc.c"
   _trec_html_escape _sse_html_escape
   _sse_html_escape2_dense
   _sse_html_escape2_aligned
